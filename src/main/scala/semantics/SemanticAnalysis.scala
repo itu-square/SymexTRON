ba9967d88af7a5088ef1ef350d45d6da5ff6b917
Ahmad Salim Al-Sibahi <asal@itu.dk>
2015-10-20 13:27:44 +0200

    Deleted unused SemanticAnalysis class

--- src/main/scala/semantics/SemanticAnalysis.scala
+++
@@ -1,9 +0,0 @@
-package semantics
-
-import syntax.ast._
-
-import scalaz._, scalaz.\/._, Scalaz._
-
-class SemanticAnalysis(defs: Map[Class, ClassDefinition]) {
-  def check(state: Map[Vars, Cardinality], stmt: Statement): String \/ Map[Vars, Cardinality] = { ??? }
-}
