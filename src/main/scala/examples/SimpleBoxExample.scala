c7f6df3400795a8513952fc4511f47c5fec9bdc8
Ahmad Salim Al-Sibahi <asal@itu.dk>
2016-03-06 20:14:39 +0100

    Added more examples that are useful for testing symbolic executor without loops.

---
+++ src/main/scala/examples/SimpleBoxExample.scala
@@ -0,0 +1,49 @@
+package examples
+
+import semantics.domains._
+import syntax.ast._
+
+/**
+  * Created by asal on 06/03/2016.
+  */
+trait SimpleBoxExample extends Example {
+
+  override val classDefs: Set[ClassDefinition] = Shared.stdClassDefs ++ Set(
+    ClassDefinition("IntBox", Map(), Map("unbox" -> (Class("Int"), Single)))
+  )
+
+  override val pres: Set[SMem] = Set(
+    SMem(Map("x" -> SetLit(Seq(Symbol(-1))), "y" -> SetLit(Seq(Symbol(-2)))),
+      SHeap.initial(Map(), Map(Symbol(-1) -> UnknownLoc(Class("IntBox"), SUnowned, Map()),
+        Symbol(-2) -> UnknownLoc(Class("IntBox"), SUnowned, Map())), Map(), Map(), Set()) )
+  )
+
+
+}
+
+object SimpleBoxSequentialLoadingExample extends SimpleBoxExample {
+  import Statement._
+
+  override val prog: Statement = stmtSeq(
+      loadField("z", SetLit(Seq(Var("x"))), "unbox"),
+      loadField("z", SetLit(Seq(Var("y"))), "unbox")
+  )
+}
+
+object SimpleBoxBranchingLoadingExample extends SimpleBoxExample {
+  import Statement._
+
+  override val prog: Statement = `if`(Eq(SetLit(Seq(Var("x"))), SetLit(Seq(Var("y")))),
+    loadField("z", SetLit(Seq(Var("y"))), "unbox")
+    , loadField("z", SetLit(Seq(Var("x"))), "unbox")
+  )
+}
+
+object SimpleBoxLadingBranchingExample extends SimpleBoxExample {
+  import Statement._
+
+  override val prog: Statement = stmtSeq(
+    loadField("z", SetLit(Seq(Var("x"))), "unbox"),
+    `if`(Eq(SetLit(Seq(Var("x"))), SetLit(Seq(Var("y")))), stmtSeq(), stmtSeq())
+  )
+}
